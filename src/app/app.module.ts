5d25ddc2aacde044d54b575d8feef2b5d0b6efe9
YounesM <younes@mgharfaoui.com>
2017-03-17 15:05:42 +0000

    Seeting example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { UserComponent } from './user/user.component';
+import { DetailsComponent } from './user/details/details.component';
+import {Routes, RouterModule} from "@angular/router";
+
+const userComponents : Routes = [
+  { path : '', component : UserComponent},
+  { path : 'details', component : DetailsComponent}
+];
+
+const routes : Routes = [
+  { path: 'user/:user', component: ParentComponent, children : userComponents}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    UserComponent,
+    DetailsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
